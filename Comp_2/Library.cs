a4187e6cd01e898757a993bd0e9032cdf8788836
Dylan <Dylanraab1@usf.edu>
2026-03-25 11:21:58 -0400

    Added Library

---
+++ Comp_2/Library.cs
@@ -0,0 +1,20 @@
+using Book;
+using Person;
+
+public class Library
+{
+    public Library()
+    {
+        
+    }
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("Displaying books in the library...");
+    }
+
+    public void DisplayPatrons()
+    {
+        Console.WriteLine("Displaying books in the library...");
+    }
+}
