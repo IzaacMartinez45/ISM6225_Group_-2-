3b10e7da65f4d6e98334bfd8656bf1ca08f58a09
Aitemir Kermaliev <akermaliev@usf.edu>
2026-03-25 11:24:44 -0400

    Update Library.cs

--- Comp_2/Library.cs
+++ Comp_2/Library.cs
@@ -1,20 +1,34 @@
-using Book;
-using Person;
+using System;
+using System.Collections.Generic;
 
 public class Library
 {
+    public List<Book> Books { get; set; }
+    public List<Person> Patrons { get; set; }
+
     public Library()
     {
-        
+        Books = new List<Book>();
+        Patrons = new List<Person>();
     }
 
     public void DisplayBooks()
     {
-        Console.WriteLine("Displaying books in the library...");
+        Console.WriteLine("Books in Library:");
+
+        foreach (var book in Books)
+        {
+            Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Available Copies: {book.AvailableCopies}");
+        }
     }
 
     public void DisplayPatrons()
     {
-        Console.WriteLine("Displaying books in the library...");
+        Console.WriteLine("Patrons in Library:");
+
+        foreach (var p in Patrons)
+        {
+            Console.WriteLine($"Name: {p.Name}, ID: {p.ID}");
+        }
     }
 }
