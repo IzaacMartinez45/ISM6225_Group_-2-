a883b678de7966d1a4d53fd5f5b8343412a8335a
usffish <ismailj@usf.edu>
2026-05-04 16:56:27 -0400

    Wire home page live market data and preview table to real watchlist data

--- StockWatchlistApp_FINAL/Controllers/HomeController.cs
+++ StockWatchlistApp_FINAL/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using StockWatchlistApp.Models;
+using StockWatchlistApp.Services;
 
-namespace BusinessNewsApp.Controllers
+namespace StockWatchlistApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly WatchlistService _watchlistService;
+        private readonly StockApiService _stockApiService;
+
+        public HomeController(WatchlistService watchlistService, StockApiService stockApiService)
         {
-            return View();
+            _watchlistService = watchlistService;
+            _stockApiService = stockApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Take up to 3 stocks from the watchlist and fetch their live data
+            var stocks = _watchlistService.GetAll().Take(3).ToList();
+
+            var preview = new List<(WatchlistStock Stock, StockApiData ApiData)>();
+
+            foreach (var stock in stocks)
+            {
+                var apiData = await _stockApiService.GetPreviousDayDataAsync(stock.Ticker);
+                preview.Add((stock, apiData));
+            }
+
+            return View(preview);
         }
 
         public IActionResult AboutUs()
